5bf7fc7c65b9e4563f047783f1bf330e042d7bac
Nicole Wakio <nicole.mulela@strathmore.edu>
2021-06-16 12:54:04 +0300

    login form added

---
+++ login.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign in page</title>
+</head>
+<body>
+    <P>sign in page</P>
+    <form  action="index.html" method='post'>
+
+        <input type="text" placeholder="username" required><br>
+        <input type="password" placeholder="password" required><br>
+        <button class="submit" type="submit" name="login" value="Sign up">login</button>
+        <p class="index">Don't have an account? <br> </p>
+        <a class="link" href="registration.php"  >Register now</a>
+      </form>
+</body>
+</html>
