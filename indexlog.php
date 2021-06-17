529919d8817b9b759d71446ecea6eeb0d4de18ae
Nicole Wakio <nicole.mulela@strathmore.edu>
2021-06-17 19:55:49 +0300

    indexlog.php update

--- indexlog.php
+++ indexlog.php
@@ -13,12 +13,14 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My website</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-    <a href="includes/logout.inc.php">Logout</a>
-    <h1>This is the index page</h1>
-    <p>Hello, <?php echo $user_data['email']; ?> </p>
-
+    
+    <div class="userContent">
+        <h1>This is the index page</h1>
+        <p>Hello, <?php echo $user_data['userName']; ?> </p>
+        <a href="includes/logout.inc.php">Logout</a>
+    </div>
 </body>
 </html>
