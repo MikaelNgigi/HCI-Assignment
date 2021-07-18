f053cc11e1423f174189c4cef9e4ad400e358e84
Nicole Wakio <nicole.mulela@strathmore.edu>
2021-07-18 22:11:44 +0300

    navigation folder updated

---
+++ navigation/auth_header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Gates N fences</title>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&family=Ubuntu:wght@700&display=swap"
+    rel="stylesheet">
+    <link rel="stylesheet" href="style/authentication.css">
+  <link rel="stylesheet" href="style/main.css">
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css">
+  <script src="https://kit.fontawesome.com/b2d1a2836c.js" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+  <script type="text/javascript"
+    src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.js"></script>
+</head>
+
+<body>
+<header>
+    <div class="main-logo-container">
+      <div class="logo">
+        <a href="index.php">
+            Gates N<br> Fences<br><span></span>
+        </a>
+      </div>
+      
+    </div>
+    <nav>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="registration.php">Sign Up</a></li>
+      </ul>
+      
+    </nav>
+  </header>
