927dc78a95405a881fb58420a82a49794303b363
Nicole Wakio <nicole.mulela@strathmore.edu>
2021-07-18 21:24:16 +0300

    index.html cnverted to index.php file

---
+++ index.php
@@ -0,0 +1,330 @@
+<?php 
+include_once 'navigation/header.php'
+?>
+  <header>
+    <div class="main-logo-container">
+      <div class="logo">
+        Gates N<br> Fences<br><span></span>
+      </div>
+      
+    </div>
+    <nav>
+      <ul>
+        <li class="active"><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#">Products</a></li>
+        <!-- <li><a href="#"></a></li> -->
+        <button class="btn button-ghost"><a href="#">Login</a></button>
+      </ul>
+      
+    </nav>
+  </header>
+  <main>
+    <section class="container" id="home">
+      <div class="home-hero grid-ish">
+        <div class="home-content">
+          <h4>Welcome to Gates N Fences</h4>
+          <div class="home-hero-carousel ">
+            <div class="home-hero-carousel-content">
+              Designed to Enhance the Entry of your home with Custom Ornamental<br>
+              decorative Driveway Gates while bringing Safety, Security and convenience.
+            </div>
+            
+          </div>
+          <button class="btn button-dark" id="about"> <a href="#">Shop Now</a></button>
+        </div>
+        <div class="home-content">
+          <img src="resources/laila-gebhard-PcdMVvTQ0zw-unsplash.jpg" alt="">
+        </div>
+      </div>
+
+    </section>
+    <section class="container" >
+      <div class="about">
+        <div class="about-content grid-ish">
+
+          <div>
+            <h1>About Us</h1>
+            Founded 35 years ago, We design and manufacture elegant and exotic gates,fences and railings.
+            With one of the most pocket friendly services you are assured of quality workmanship. We offer a wide
+            selection of designs in aluminium or wrought iron metals.
+            Our commitment to our customers and dedication to produce quality gates has earned us thousands of satisfied
+            customers.
+            <br>
+          </div>
+          <img src="resources/about-1.jpg" alt="Welding" class="about-img">
+        </div>
+      </div>
+    </section>
+
+    <section class="container">
+      <div class="testimonial">
+        <h1>Testimonials</h1>
+
+        <div class="testimonial-carousel">
+          <div class="testimonial-card">
+            <div class="testimonial-content">
+              <img
+                src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80"
+                alt="customer" class="testimonial-img">
+              <i class="fas fa-quote-right"></i>
+            </div>
+            <div class="testimonial-content">
+              <p class="testimonial-desc">
+                I was very happy with the service and the price was $100 lower than anyone else. I would not hesitate to
+                use them again.
+              </p>
+              <span>Allan.K</span>
+              <span>Customer</span>
+            </div>
+          </div>
+          <div class="testimonial-card">
+            <div class="testimonial-content">
+              <img
+                src="https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80"
+                alt="customer" class="testimonial-img">
+              <i class="fas fa-quote-right"></i>
+            </div>
+            <div class="testimonial-content">
+              <p class="testimonial-desc">
+                The service was good.
+              </p>
+              <span>Tatyana D.</span>
+              <span>Customer</span>
+            </div>
+          </div>
+          <div class="testimonial-card">
+            <div class="testimonial-content">
+              <img
+                src="https://images.unsplash.com/photo-1581599129568-e33151627628?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"
+                alt="customer" class="testimonial-img">
+              <i class="fas fa-quote-right"></i>
+            </div>
+            <div class="testimonial-content">
+              <p class="testimonial-desc">
+                Awesome experience....professional, prompt and nice in addition!!!!! Awesome sevice!!!!!!!!
+              </p>
+              <span>J T.</span>
+              <span>Customer</span>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </section>
+    <section class="container">
+      <div class="cta">
+        <h1> 35 Years of experience<br> to assure quality.</h1>
+      </div>
+    </section>
+    <section class="container">
+      <div class="products">
+        <h1>Our products</h1>
+        <div class="product-card">
+          <img src="https://images.unsplash.com/photo-1583849159801-d85cf57aea27?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=335&q=80" alt="railing" class="product-img">
+          <div class="product-desc">
+            <h3>Railing</h3>
+          </div>
+        </div>
+        <div class="product-card">
+          <img src="https://images.unsplash.com/photo-1612044522649-9be0b77b9619?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" alt="garden gates" class="product-img">
+          <div class="product-desc">
+            <h3>Garden gates</h3>
+          </div>
+        </div>
+        <div class="product-card">
+          <img src="https://images.unsplash.com/photo-1557478317-178fcf4341ac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80" alt="pool fences" class="product-img">
+          <div class="product-desc">
+            <h3>Pool fences</h3>
+          </div>
+        </div>
+        <div class="product-card">
+          <img src="https://images.unsplash.com/photo-1519973759984-cf5a6c557cd8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="driveway gates" class="product-img">
+          <div class="product-desc">
+            <h3>Scissor gates</h3>
+          </div>
+        </div>
+        <div class="product-card">
+          <img src="https://images.unsplash.com/photo-1588362951121-3ee319b018b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" alt="access control" class="product-img">
+          <div class="product-desc">
+            <h3>Access Control</h3>
+          </div>
+        </div>
+        <div class="product-card">
+          <img src="https://images.unsplash.com/photo-1580706483913-b6ea7db483a0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=637&q=80" alt="Main control circuit boards" class="product-img">
+          <div class="product-desc">
+            <h3>Circuit boards</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="container">
+      <h1>Our products are from trusted sources</h1>
+      <div class="manufacturers">
+        
+        <div class="logo-container"><img src="resources/doorking-logo.png" alt="doorking logo"></div>
+        <div class="logo-container"><img src="resources/vikings-logo.png" alt="vikings Logo"></div>
+        <div class="logo-container"><img src="resources/miller-edge.png" alt=""></div>
+        <div class="logo-container"><img src="resources/emx-logo.png" alt=""></div>
+        <div class="logo-container"><img src="resources/aas-logo.png" alt=""></div>
+        <div class="logo-container"><img src="resources/powermaster-logo.png" alt=""></div>
+        <div class="logo-container"><img src="resources/chamberlain.png" alt=""></div>
+        <div class="logo-container"><img src="resources/ramset-logo.png" alt=""></div>
+        <div class="logo-container"><img src="resources/faac-logo.png" alt=""></div>
+        <div class="logo-container"><img src="resources/secura-logo.png" alt=""></div>
+        <div class="logo-container"><img src="resources/multicode-logo.png" alt=""></div>
+        <div class="logo-container"><img src="resources/select-logo.png" alt=""></div>
+        <div class="logo-container"></div>
+        
+      </div>
+    </section>
+    <section class="container">
+      <div class="benefits-list">
+        <h1>Why Choose our products</h1>
+        <div class="benefits-container">
+          <div class="benefit-card">
+            <img src="https://image.flaticon.com/icons/png/512/1027/1027841.png" alt="cheap" class="benefit-icon">
+            <p>Cheaper</p>
+          </div>
+          <div class="benefit-card">
+            <img src="https://image.flaticon.com/icons/png/512/4264/4264133.png" alt="feather" class="benefit-icon">
+            <p>Light</p>
+          </div>
+          <div class="benefit-card">
+            <img src="https://image.flaticon.com/icons/png/512/1017/1017568.png" alt="durable" class="benefit-icon">
+            <p>Durable</p>
+          </div>
+          <div class="benefit-card">
+            <img src="https://image.flaticon.com/icons/png/512/2913/2913136.png" alt="" class="benefit-icon">
+            <p>Elegant</p>
+          </div>
+          <div class="benefit-card">
+            <img src="https://image.flaticon.com/icons/png/512/1077/1077198.png" alt="" class="benefit-icon">
+            <p>Low maintenance</p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    
+    <section class="container">
+      <div class="cta" id="services">
+        <h1>Need Help? Get Free quote!</h1>
+        <button class="btn button-ghost"><a href="#">Contact Us</a> </button>
+      </div>
+    </section>
+    <section class="container">
+      <div class="services" >
+        <h1>Other Services </h1>
+        <div class="benefits-container">
+          <div class="benefit-card">
+            <img src="https://image.flaticon.com/icons/png/512/846/846296.png" alt="cheap" class="benefit-icon">
+            <p>
+              Car Care<br>
+              <a href="http://www.rrautorepairs/">Learn more <i class="fas fa-arrow-right"></i></a>
+            </p>
+          </div>
+          <div class="benefits-container">
+            <div class="benefit-card">
+              <img src="https://image.flaticon.com/icons/png/512/3648/3648953.png" alt="cheap" class="benefit-icon">
+              <p>
+                Hydroponics<br>
+                <a href="http://www.hydroponics-grow-light.com/HYDROPONICS/American-Hydroponics_Hydroponics_Hydroponics-System_Grow-Hydroponics.html">Learn more <i class="fas fa-arrow-right"></i></a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="conatiner" id="contact">
+      <div class="contact">
+        <div class="contact-content grid-ish">
+          <div class="map-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3590.653001834473!2d-80.26069738533643!3d25.84797711133336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9b08c694c9b4f%3A0x34288a3ec7e713d4!2sL%20A%20Ornamental%20%2F%20Gate%20Operators!5e0!3m2!1sen!2ske!4v1623742564159!5m2!1sen!2ske" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          </div>
+          <div class="contact-info">
+            <h1>Contact Us</h1>
+            <div class="even-columns">
+              <i class="fas fa-map-marker-alt"></i>
+              L.A. Ornamental Corp <br>
+            3708 N.W. 82nd Street
+            Miami, Florida 33147
+            </div>
+            <div class="even-columns">
+              <i class="fas fa-envelope"></i>
+              <a href="mailto:LAOrnamental@aol.com">LAOrnamental@aol.com</a>
+            </div>
+            <div class="even-columns">
+              <i class="fas fa-map-marker-alt"></i>
+              <a href="tel:305-696-0419">305-696-0419</a>
+            </div>
+            <div class="even-columns">
+              <i class="fas fa-globe"></i>
+              <a href="http://www.gatesnfences.com/index.html">www.gatesnfences.com</a>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="container">
+      <form action="" class="contact-form">
+        <h1>Leave a Message</h1>
+        <input type="text" name="name" id="" placeholder="Your name" required>
+        <input type="tel" name="telephone" id="" placeholder="phone number" required>
+        <input type="email" required placeholder="your email">
+        <textarea name="message" id="" cols="30" rows="10" placeholder="Your message..."></textarea>
+        <input type="submit" value="Send message" class="btn button-dark">
+      </form>
+    </section>
+
+  </main>
+  <footer class="grid-ish">
+    <div class="footer-about">
+      <div class="logo">
+        Gates N<br> Fences<br><span></span>
+      </div>
+      <h2>gates N Fences</h2>
+      With one of the most pocket friendly services you are assured of quality workmanship. We offer a wide selection of designs in aluminium or wrought iron metals. 
+      Our commitment to our customers and dedication to produce quality gates has earned us thousands of satisfied customers
+    </div>
+    <div class="footer-links">
+      <h2>Quick Links</h2>
+      <a href="#home">Home</a>
+      <a href="#About">About</a>
+      <a href="#">products</a>
+      <a href="#services">services</a>
+    </div>
+    <div class="resources">
+      <h2>Resources</h2>
+      <a href="http://www.gatesnfences.com/our_policies.html">Our policies</a>
+      <a href="http://www.gatesnfences.com/FAQ_html.html">FAQ</a>
+    </div>
+    <div class="footer-social">
+      <h2>Contact us</h2>
+      <div class="even-columns">
+        <i class="fas fa-map-marker-alt"></i>
+        L.A. Ornamental Corp <br>
+      3708 N.W. 82nd Street
+      Miami, Florida 33147
+      </div>
+      <div class="even-columns">
+        <i class="fas fa-envelope"></i>
+        <a href="mailto:LAOrnamental@aol.com">LAOrnamental@aol.com</a>
+      </div>
+      <div class="even-columns">
+        <i class="fas fa-map-marker-alt"></i>
+        <a href="tel:305-696-0419">305-696-0419</a>
+      </div>
+      <div class="even-columns">
+        <i class="fas fa-globe"></i>
+        <a href="http://www.gatesnfences.com/index.html">www.gatesnfences.com</a>
+      </div>
+    </div>
+  </footer>
+  <script src="app.js"></script>
+  <?php 
+include_once 'navigation/footer.php'
+?>
